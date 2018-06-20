bf4c0683a6ca1f138fc34e2c29cb7eac5602d4cd
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2018-06-20 09:14:04 +0100

    Add files via upload
    made changes on code 
    - added attr_reader
    - made the variables names more readable

--- spec.rb
+++ spec.rb
@@ -13,18 +13,22 @@
 # make them pass. Then move on to the other two methods, but this time you'll
 # create the descriptions to match the requirements above.
 class AddressBook
+  attr_reader :first_line, :city, :postcode
+
   def initialize(first_line, city, postcode)
     @first_line = first_line
     @city = city
     @postcode = postcode
   end
 
-  def full_name_add
-    "#{@first_line} #{@city} #{@postcode}".split.join(' ')
+  def address
+    "#{first_line} #{city} #{postcode}".split.join(' ')
   end
 end
 ###
 class Person
+  attr_reader :first_name, :middle_name, :last_name
+
   def initialize(first_name:, middle_name: nil, last_name:)
     @first_name = first_name
     @middle_name = middle_name
@@ -34,7 +38,7 @@ class Person
   # implement your behavior here
 
   def full_name
-    "#{@first_name} #{@middle_name} #{@last_name}".split.join(' ')
+    "#{first_name} #{middle_name} #{last_name}".split.join(' ')
   end
 
   def add_contact(contact)
@@ -42,52 +46,53 @@ class Person
   end
 
   def full_name_with_middle_initial
-    if @middle_name.nil?
-      @first_name + ' ' + @last_name
+    if middle_name.nil?
+      first_name + ' ' + last_name
     else
-      @first_name + ' ' + @middle_name[0] + '. ' + @last_name
+      first_name + ' ' + middle_name[0] + '. ' + last_name
     end
   end
 
   def initials
-    "#{@first_name} #{@middle_name} #{@last_name}".split.map(&:chr).join
+    "#{first_name} #{middle_name} #{last_name}".split.map(&:chr).join
   end
 end
 
 RSpec.describe Person do
-  let(:person_full) { Person.new(first_name: 'John', middle_name: 'Lee', last_name: 'Cooper') }
-  let(:person_fl) { Person.new(first_name: 'John', last_name: 'Cooper') }
-  let(:add) { AddressBook.new('345', 'London', 'EC1290') }
+  let(:person_fullname) { Person.new(first_name: 'John', middle_name: 'Lee', last_name: 'Cooper') }
+  let(:person_name) { Person.new(first_name: 'John', last_name: 'Cooper') }
+  let(:address_book) { AddressBook.new('345', 'London', 'EC1290') }
+
   describe '#full_name' do
-    let(:add_name) { 'John Lee Cooper 345 London EC1290' }
-    let(:add_nm) { 'John Cooper 345 London EC1290' }
+    let(:address_fullname) { 'John Lee Cooper 345 London EC1290' }
+    let(:person_address) { 'John Cooper 345 London EC1290' }
     it 'concatenates first name, middle name, and last name with spaces' do
-      expect(person_full.full_name).to eq('John Lee Cooper')
-      expect(person_full.add_contact(add.full_name_add)).to eq(add_name)
+      expect(person_fullname.full_name).to eq('John Lee Cooper')
+      expect(person_fullname.add_contact(address_book.address)).to eq(address_fullname)
     end
     it 'does not add extra spaces if middle name is missing' do
-      expect(person_fl.full_name).to eq('John Cooper')
+      expect(person_name.full_name).to eq('John Cooper')
     end
     it 'add contact with name' do
-      expect(person_fl.add_contact(add.full_name_add)).to eq(add_nm)
+      expect(person_name.add_contact(address_book.address)).to eq(person_address)
     end
   end
 
   describe '#full_name_with_middle_initial' do
     it 'concatenates first_name and last_name with a middle_name initial' do
-      expect(person_full.full_name_with_middle_initial).to eq('John L. Cooper')
+      expect(person_fullname.full_name_with_middle_initial).to eq('John L. Cooper')
     end
     it 'does not add extra spaces or a period if middle_name is missing' do
-      expect(person_fl.full_name_with_middle_initial).to eq('John Cooper')
+      expect(person_name.full_name_with_middle_initial).to eq('John Cooper')
     end
   end
 
   describe '#initials' do
     it 'return all initial for first_name , middle_name and last_name ' do
-      expect(person_full.initials).to eq('JLC')
+      expect(person_fullname.initials).to eq('JLC')
     end
     it 'only return two characters if middle_name is missing' do
-      expect(person_fl.initials).to eq('JC')
+      expect(person_name.initials).to eq('JC')
     end
   end
 end
